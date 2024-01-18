2e89292a744a35e392defa2156eb1464fe82c7ba
Ali-Hasan-Khan <alihasank86@gmail.com>
2024-01-18 21:07:55 +0530

    added template

---
+++ template.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+using li = long int;
+using lli  = long long int;
+
+
+#define loop(x,n) for(int x = 0; x < n; ++x)
+#define loop1S(x,n) for(int x = 1; x <= n; ++x)
+#define revloop(x,n) for(int x = n-1; x>=0; --x)
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define all(v) (v).begin(),(v).end()
+#define in(v) for(auto& item:v) cin >> item;
